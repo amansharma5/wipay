c0729a683f34690c75bdd2e184dbac51eac1ee2c
Aman sharma <amansharma5@hotmail.com>
2020-05-05 01:19:39 +0530

    wipay payment Wrapper

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wipay
 Wipay payment Integration
+
+The wipay is a node library which provides easy access to the Wipay API from application written in server-side JavaScript.
+
+Documentation
+
+See the Documentation at wipay documentation
---
+++ app.js
@@ -0,0 +1,50 @@
+var request = require('http');
+
+var payment_url = {
+    "Live" : "https://wipayfinancial.com/v1/gateway_live",
+    "Sandbox" : "https://sandbox.wipayfinancial.com/v1/gateway"
+}
+
+module.exports = {
+    mode : "Sandbox",
+
+    isProdMode: (mode)=> {
+        if (mode) {
+            this.mode = 'Live';
+        } else {
+            this.mode = 'Sandbox';
+        }
+    },
+
+    makePayment: async(data)=> {
+        return await new Promise((resolve, reject)=>{
+            try {
+                var params = {
+                    total : data.total,
+				    phone : data.phone,
+				    email : data.email,
+				    name : data.name,
+				    order_id : data.order_id,
+				    payment_id: data.payment_id,
+				    transaction_id:data.transaction_id,
+				    return_url : data.return_url,
+				    developer_id : data.developer_id || "1"
+                }
+
+            request.post({url : payment_url[this.mode], params }, function(error, response, body) {
+                if (!error) {
+                    var result = response.headers.location;
+                    resolve(body);
+                    }
+                else{
+                    reject(error.message);
+                }
+            });
+            } catch (error) {
+                reject(error.message)
+            }
+        })
+        
+    }
+}
+
